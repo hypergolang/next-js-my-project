80993e7027b9f448c39199acda311fa4a4cdb6ee
hypergolang <hypergolang@gmail.com>
2025-08-07 18:46:36 +0700

    feat: Create PrintForge home page with basic layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
+/**
+ * Challenge: Create the PrintForge Home Page
+ * 
+ * For this (and all future challenges, unless I say otherwise),
+ * I won't expect you to put much effort into the CSS of the 
+ * challenges.
+ */
+
 export default function Home() {
-    return
+    return (
+        <main>
+            <h1>Discover what's possible with 3D printing</h1>
+            <p>Join our community of creators and explore a vast
+                library of user-submitted models.</p>
+
+            <button>Browse Models</button>
+            <img src="/home-page-hero-img.png" />
+        </main>
+    )
 }
